f1ec37f2f0ea6ed53024ebd3e2249a329f8800fe
Bozhidar Zahariev <bozhidar_zahariev@hotmail.com>
2019-10-12 12:45:48 +0100

    working

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//HOME
 class Title extends React.Component {
   constructor(props) {
     super(props);
@@ -25,6 +26,7 @@ class Middle extends React.Component {
   }
 }
 
+/*
 class Icons extends React.Component {
   render() {
     return (
@@ -35,16 +37,158 @@ class Icons extends React.Component {
     );
   }
 }
+*/
 
-class BigContainer extends React.Component {
+/*
+class HomeScreen extends React.Component {
   render() {
     return (
-      <div className="big-container">
+      <div className="home-screen">
         <Title title="Vodolaza" smallTitle="The best Pub & Fish Restaurant in Plovdiv" />
         <Middle />
         <Icons textLeft="Menu" textRight="Find us" />
       </div>
     );
+  }
+}
+*/
+
+//MENU
+class MenuBackground extends React.Component {
+  render() {
+    return (
+      <div className="menu-background">
+        <div className="menu">
+          <span className="menu-item">Rakia</span>
+          <span className="menu-price">1.80 lv</span>
+          <span className="menu-item">Vodka</span>
+          <span className="menu-price">1.80 lv</span>
+          <span className="menu-item">Whisky</span>
+          <span className="menu-price">2.00 lv</span>
+          <span className="menu-item">Chips</span>
+          <span className="menu-price">1.20 lv</span>
+          <span className="menu-item">Meatball</span>
+          <span className="menu-price">0.80 lv</span>
+          <span className="menu-item">Sprat</span>
+          <span className="menu-price">1.60 lv</span>
+          <span className="menu-item">Carp</span>
+          <span className="menu-price">3.00 lv</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+/*
+class MenuScreen extends React.Component {
+  render() {
+    return (
+      <div className="menu-screen">
+        <MenuBackground />
+        <Icons textLeft="Menu" textRight="Find us" />
+      </div>
+    );
+  }
+}
+*/
+
+class FindUs extends React.Component {
+  render() {
+    return (
+      <div className="find-us">
+        <p className="address"> ul. "Mladezhka" 32А, Plovdiv, Bulgaria</p>
+
+        <div className="map"></div>
+
+        <p className="hours">Opening Hours: 9:30am–9:30pm</p>
+      </div>
+    );
+  }
+}
+
+class BigContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 'home'
+    };
+
+    this.toggleDisplayLeft = this.toggleDisplayLeft.bind(this);
+    this.toggleDisplayRight = this.toggleDisplayRight.bind(this);
+  }
+
+  toggleDisplayLeft() {
+    if (this.state.display === 'home' || this.state.display === 'map') {
+      this.setState({
+        display: 'menu'
+      });
+    } else if (this.state.display === 'menu') {
+      this.setState({
+        display: 'home'
+      });
+    }
+  }
+
+  toggleDisplayRight() {
+    if (this.state.display === 'home' || this.state.display === 'menu') {
+      this.setState({
+        display: 'map'
+      });
+    } else if (this.state.display === 'map') {
+      this.setState({
+        display: 'home'
+      });
+    }
+  }
+
+  render() {
+    if (this.state.display === 'home') {
+      return (
+        <div className="big-container">
+          <Title title="Vodolaza" smallTitle="The best Pub & Fish Restaurant in Plovdiv" />
+          <Middle />
+
+          <div className="icons">
+            <span className="icon-left" onClick={this.toggleDisplayLeft}>
+              Menu
+            </span>
+            <span className="icon-right" onClick={this.toggleDisplayRight}>
+              Find us
+            </span>
+          </div>
+        </div>
+      );
+    } else if (this.state.display === 'menu') {
+      return (
+        <div className="big-container">
+          <MenuBackground />
+
+          <div className="icons">
+            <span className="icon-left" onClick={this.toggleDisplayLeft}>
+              Home
+            </span>
+            <span className="icon-right" onClick={this.toggleDisplayRight}>
+              Find us
+            </span>
+          </div>
+        </div>
+      );
+    } else if (this.state.display === 'map') {
+      return (
+        <div className="big-container">
+          <FindUs />
+
+          <div className="icons">
+            <span className="icon-left" onClick={this.toggleDisplayLeft}>
+              Menu
+            </span>
+            <span className="icon-right" onClick={this.toggleDisplayRight}>
+              Home
+            </span>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
